c1ab3e43b6239d2110fa5d664ad13b600d378c18
WalshM27 <WalshM27@chcstudent.com>
2025-09-10 14:15:26 -0400

    commit

---
+++ variables.py
@@ -0,0 +1,19 @@
+x=5
+y=10
+z=0
+sum = x + y
+
+name= "Micha Morozov" 
+occupation="student"
+first = "Calvert"
+second = "Hall"
+school = first + second
+print("Welcome to calvert hall!")
+name = input("What is your name? ")
+#Variable = input(<prompt>) 
+gradyear = input("What year do you graduate? ")
+standing = input("What is your class standing? ")
+email = input("What is your email? ")
+print(f"My name is {name}, I graduate in {gradyear}, my class standing is {standing}, and my email is {email}")
+print(name)
+print(occupation)
