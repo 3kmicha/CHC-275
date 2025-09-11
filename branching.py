234457593280a365b0e563ca1ef15618c0f2770d
EvilExtractor13 <SadlerA27@chcstudent.com>
2025-09-11 09:29:26 -0400

    commit

---
+++ branching.py
@@ -0,0 +1,23 @@
+
+x = 10 
+if x % 2 == 0: 
+    print(f"{x} is divisible by 2")
+    print("test")
+else:    
+    print("this is not apart of the if statement")
+    
+if x % 2 == 1: 
+    print(f"{x} is divisible by 2")
+    print("test")
+   
+print("this is not apart of the if statement")
+
+
+
+x = 10 
+if x % 5 ==0:
+    print(f"{x} is divisable by 5")
+elif x % 2 ==0:
+    print(f"{x} is divisable by 2")
+else: 
+    print(f"{x}is not divisable by 5 or 2")
---
+++ calculator.py
@@ -0,0 +1,29 @@
+x=input("num1")
+y=input("num2")
+x=int(x)
+y=int(y)
+sum=x+y 
+print(f"the sum of {x} + {y} = {sum}")
+
+a=input("num3")
+b=input("num4")
+a=int(a)
+b=int(b)
+difference=a-b 
+print(f"the difference {a} - {b} = {difference}")
+
+x=input("num1")
+y=input("num2")
+x=int(x)
+y=int(y)
+product=x*y 
+print(f"the product of {x} * {y} = {product}")
+
+a=input("num3")
+b=input("num4")
+a=int(a)
+b=int(b)
+quotient=a-b 
+print(f"the quotient {a} / {b} = {quotient}")
+
+10123132*2
