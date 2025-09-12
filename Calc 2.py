e9a4fec9eaeaa2153e0d533522fa2062e5fa8c58
WalshM27 <WalshM27@chcstudent.com>
2025-09-12 14:18:38 -0400

    commit

---
+++ Calc 2.py
@@ -0,0 +1,29 @@
+print("welcome to calculator")
+
+
+print("1. Addition")
+print("2. Subtraction")
+print("3. Multiplication")
+print("4. Division")
+
+math =input("enter your math:")
+
+if math == "Addition":
+    x=input("num1:")
+    y=input("num2:")
+    x=int(x)
+    y=int(y)
+    sum=x+y 
+    print(f"the sum of {x} + {y} = {sum}")
+elif math == "Subtraction":
+    print("Your grade level is 10")
+elif math == "Multiplication":
+    print("Your grade level is 11")
+elif math == "Division":
+    print("Your grade level is 12")
+    
+
+
+
+
+
--- branching.py
+++ branching.py
@@ -21,3 +21,19 @@ elif x % 2 ==0:
     print(f"{x} is divisable by 2")
 else: 
     print(f"{x}is not divisable by 5 or 2")
+    
+x = 10 
+if x > 5 or x > 15: 
+    print(f"{x} is greater than 5 or 15")
+    
+year = input("enter your year:")
+
+if year == "freshman":
+    print("Your grade level is 9")
+elif year == "sophomore":
+    print("Your grade level is 10")
+elif year == "junior":
+    print("Your grade level is 11")
+elif year == "senior":
+    print("Your grade level is 12")
+    
