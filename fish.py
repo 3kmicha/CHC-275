319f14061edcde4f16f02dc69c4ac9c9f49c9cda
WalshM27 <johnst27@chcstudent.com>
2025-09-19 13:34:39 -0400

    commit

---
+++ fish.py
@@ -0,0 +1,9 @@
+
+option = input("Enter your fish type:")
+if option == "carniviorous":
+    x=input("Do you already have it?:")
+    if x == "yes":
+        print("too bad")
+
+elif x == "no":
+        print("enjoy")
---
+++ hailstone.py
@@ -0,0 +1,24 @@
+""""The hailstone problem (collatz conjecture) is an open problem in the field of number theory in math.
+
+(all of this is inside of a while loop.) (exit condition  == 1)
+ - Take any integer (x) (use input and typecasting )
+ - if it's odd you multiply it by 3 and add 1 (relevant skills % and if statements) (any odd number divided by 2 has a remainder of 1)
+ - if it's even, you divide by 2 (number mod 2 == 0)
+ 
+The problem statement is:
+
+- every number eventually ends in a loop of 4-2-1
+
+
+67 after 3*67+1
+202 after 101
+304 after 152
+152 after 76
+76 after 38
+38 after 19
+19 odd 58
+58
+
+
+your goal, using while loops and an int variable, print out the path a number takes to hit the 4-2-1"""""
+
