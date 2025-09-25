0be853d49508c0bf4cb7175b74a0833fc15ec289
WalshM27 <johnst27@chcstudent.com>
2025-09-25 09:29:41 -0400

    commit

---
+++ list.py
@@ -0,0 +1,17 @@
+
+
+mylist = [5,10,15,20,25,30,35,40,]
+print(mylist)
+
+i = 0
+while i <= len(mylist) - 1:
+    (mylist[i]) = mylist[i] + 5
+    i = i + 1
+    
+print(mylist)   
+    
+mylist = [5,10,15,20,25,30,35,40,]
+for num in mylist:
+    num = num + 5
+
+print(mylist)
