2739cace2a298e26739375de02b64c9b0c4c2b2b
WalshM27 <johnst27@chcstudent.com>
2025-10-07 14:12:04 -0400

    commit

--- Bank.py
+++ Bank.py
@@ -13,7 +13,7 @@ while check == False:
         print("6. Remove Account")
         
         action = input("What would you like to do today?:")
-        
+        # Funnest code to create. The .index fuction takes the exact acctount I would like to add money to and just adds it. Didn't take super long to make, and helped me code basically everything else in this program. 
         if action == "Deposit":
             actname = input("Which account would like to Deposit into?:")
             money = input("how much would you like to Deposit?:")
@@ -32,6 +32,10 @@ while check == False:
             actbalances[index] = actbalances[index] - money
             print("Thank you for banking with First Financial Credit Union! Have a wonderful day!" )
             
+            
+            
+            # This was the most confusing code I had to put together. This lets me make a transfer from one account to another by using the .index fuction to pull the exact postion of the account I would like to transfer
+            # from and to from the list that I created 
         elif action == "Transfer":
             actname = input("Which account would like to Transfer from?:")
             actname2 = input("Which account would like to Transfer to?:")
@@ -43,20 +47,29 @@ while check == False:
             actbalances[index2] = actbalances[index2] + money
             print("Thank you for banking with First Financial Credit Union! Have a wonderful day!" )
             
+        
+            #The quickest code to make, took like 10 seconds to figure out, not a whole lot going on here, literallyt just printing everything in my list.
         elif action == "List All Accounts":
             print(len(actnames))
+            
+            
             
         elif action == "Add Account":
           actname = input("Which account would like to Add?:")
           actnames.append(actname)
           actbalances.append(0)
           
+          
+          
+          # This was also pretty confusing.The .pop fuction allows me to remove the element from the exact postion in my list while also remvoing the balnce, something .remove would not do.  
         elif action == "Remove Account":
             actname = input("Which account would like to Remove?:")
             index= actnames.index(actname)
             actnames.pop(index) 
             actbalances.pop(index)
             print("Thank you for banking with First Financial Credit Union! Have a wonderful day!" )
+            
+            
             
         elif action == "quit":
             check = True
