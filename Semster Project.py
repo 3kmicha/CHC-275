8f0a732cc1dbdf4b418d7d931033ee0c790c152c
WalshM27 <johnst27@chcstudent.com>
2025-12-04 10:54:49 -0500

    c

---
+++ Semster Project.py
@@ -0,0 +1,10 @@
+print ("Welcome to the World's Travel Guide")
+name = input("what is your name?")
+print (f"Welcome{name} lets find you a place to travel too!")
+
+print("First lets start off with what you are looking for!")
+print("Please fill this question sheet with what you would like to find in your destination")
+
+continent = input("What continent would you like to visit?")
+climate  = input("What climate type are you looking for?")
+language = input("What language would you prefer?")
