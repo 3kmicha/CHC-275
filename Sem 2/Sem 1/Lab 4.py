7a2c334ec1a6f1c396219f7e7a9c4817cce0018b
3kmicha <MorozovM27@chcstudent.com>
2026-02-10 10:15:06 -0500

    commit

--- Sem 2/Sem 1/Lab 4.py
+++ Sem 2/Sem 1/Lab 4.py
@@ -1,31 +1,17 @@
-
 import math
 
+def makeList():
+    nums = []
+    check = False
+    while check == False:
+        option = input("Enter a list of intergers or 'q' to end the lsit:")
+        if option == "quit".lower():
+            check = True
+        else: 
+            option = int(option)
+            nums.append(option)
+    return(nums)
 
-"""
-Function Name: getList
-Parameters: none
-Return Type: List
-Description: Prompts user to fill in an empty list until they are satisfied 
-"""
-def getList():
-    print("Welcome to the List Statistics Calculator")
-    print("Enter a list of intergers or 'q' to end the lsit")
-    print("Enter an interger: 1")
-    print("Enter an interger: 2")
-    print("Enter an interger: 3")
-    print("Enter an interger: 4")
-    print("Enter an interger: 5")
-    print("Enter an interger: q")
-    print("Please Choose the Statistic you would like to calculate")
-    return list
-
-""" 
-Function Name: printMenu
-Parameters: none
-Return Type: none
-Description: Prints menu for statistics calculator
-"""
 def printMenu():
     print("Please Choose the Statistic you would like to calculate")
     print("1. Min")
@@ -33,7 +19,7 @@ def printMenu():
     print("3. Mean")
     print("4. Median")
     print("5. Clear List")
-    
+""""""    
 
 
 """
@@ -43,16 +29,7 @@ Return Type: Float
 Description: Calculates the mean for the list and returns the value 
 """
 def getMean(userList):
-    sum = 0 
-mylist = [17,64,16,71,13,92,97,98,31,60,83,2,84,68,62,22,24,76,95,55,6,59,10,29,35,85,26,4,1,53,12,61,20,77,89,5,94,74,25,81,70,82,73,18,15,46,69,3,87,66,23,80,42,21,7,65,9,
-0,36,96,52,79,32,39,1,33,40,50,14,48,45,51,27,34,30,100,88,9,63,86,19,78,54,47,57
-,49,67,91,56,44,58,72,43,38,11,37,75,4,28,99,8,93]
-for num in mylist:
-    sum = sum + num     
-mean = sum / len(mylist)
-print(mean)
-
-
+    pass
 
 """
 Function Name: getMedian
@@ -93,7 +70,15 @@ def getStdDev(userList):
 
 
 def main():
-    pass
+    print("Welcome to the List Statistics Calculator")
+    list1 = makeList()
+    print(list1)
+    check = False
+    while check == False:
+        printMenu()
+        option = input("Select what you want top do:")
+        if option == "q":
+            check = True
 
 
 if __name__ == "__main__":
