0059b571a25ae5602e7e7da82511775384753b34
3kmicha <MorozovM27@chcstudent.com>
2026-03-11 09:23:19 -0400

    commit

--- Sem 2/Sem 1/Dictionary lsit.py
+++ Sem 2/Sem 1/Dictionary lsit.py
@@ -10,16 +10,18 @@ But it's not the best way to do it
 Lets use a dictionary 
 """
 
+
 products = {"oranges":0, "apples" :5, "grapes":10,}
-
 "To retieave info use sqruare brackets "
-
 print(f"oranges: {products["oranges"]}")
+
+
 "Below isnt the best way to create list cause what happens when something is removed, now everything is out of place"
 productsList = [1,2,3,4,5,6]
 print(productsList[3])
 productsList.pop(3)
 print(productsList[3])
+
 
 "Adding to the dictionary"
 products ["bananas"] = 30
@@ -28,17 +30,55 @@ products [1] = 60
 print(f"1: {products[1]}") 
 print(products)
 
+
 "Keys = The fruit"
 "Values = The numbers"
 "Looping with Dictionaires"
 
+
+"This prints the values"
 for x in products:
     print(products[x])
-"This prints the values"
 
+
+"This prints the keys"
+"Keys are imutable meaning they can only be strings, numbers, and tuples"
 for x in products:
     print(x)
 for names in products.keys():
     print(names)
-"This prints the keys"
 
+"This is how you remove stuff from dictionaires"
+mydict = {1:"foo",2:"foot",3:"food",4:"fold",}
+print(mydict)
+"This is the unsafe way of doing it using del[]"
+del mydict[1]
+print(mydict)
+
+
+"here is the safe way to do it using .pop()"
+mydict.pop(2)
+
+"heres how to copy, it just makes copies"
+copy = mydict
+print(copy)
+
+
+"If you want to delete something but its already not there you can use this"
+print(mydict.pop(2, "object not found"))
+
+
+"Just clears everything within the list"
+mydict.clear 
+
+
+"New data type: Tuple's"
+"Use an ordered pair notation like (x,y)"
+"These are imutable"
+
+mytuple = (10,20,30)
+print(mytuple)
+
+students = {}
+students[(1,"John Smith")] = {"english": 80, "math": 93, "econ": 71}
+print(students)
